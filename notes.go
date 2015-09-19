14dbadb7b6e58bca6cdbb97a173cd00fb1a54894
Lucas Stadler <lu@papill0n.org>
2015-09-19 10:07:50 +0200

    Move pinboard import to a subcommand

--- notes.go
+++ notes.go
@@ -29,9 +29,22 @@ type Post struct {
 }
 
 func main() {
-	err := ImportFromPinboard(os.Args[1], "files://db?name=posts")
-	if err != nil {
-		panic(err)
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <cmd> [<args>]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+	args := os.Args[2:]
+	switch cmd {
+	case "import":
+		err := ImportFromPinboard(args[0], "files://db?name=posts")
+		if err != nil {
+			panic(err)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command '%s'\n", cmd)
+		os.Exit(1)
 	}
 }
 
