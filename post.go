e4ff740ad2ea44baadd88fb0ca5c5c81164b9129
Lucas Stadler <lu@papill0n.org>
2015-10-01 21:23:07 +0200

    Support encoding posts as JSON
    We have to do it ourselves, because we access the fields via methods,
    not as fields on the `post` struct itself.

--- post.go
+++ post.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/heyLu/mu"
 	"github.com/heyLu/mu/database"
 	"net/url"
@@ -46,6 +48,34 @@ func (p Post) Tags() []Tag {
 		tags[i] = Tag{rawTag.(database.Entity)}
 	}
 	return tags
+}
+
+func (p Post) MarshalJSON() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	fmt.Fprintf(buf, "{")
+	fmt.Fprintf(buf, "\"id\": %q, ", p.Id())
+	fmt.Fprintf(buf, "\"title\": %q, ", p.Title())
+	fmt.Fprintf(buf, "\"content\": %q, ", p.Content())
+	fmt.Fprintf(buf, "\"date\": \"%s\"", p.Date().Format(time.RFC3339))
+	u := p.URL()
+	if u != nil {
+		fmt.Fprintf(buf, " ,\"url\": %q", u)
+	}
+	tags := p.Tags()
+	if tags != nil {
+		fmt.Fprint(buf, " ,\"tags\": [")
+		first := true
+		for _, tag := range tags {
+			if !first {
+				fmt.Fprint(buf, ", ")
+			}
+			first = false
+			fmt.Fprintf(buf, "%q", tag)
+		}
+		fmt.Fprintf(buf, "]")
+	}
+	buf.WriteString("}")
+	return buf.Bytes(), nil
 }
 
 type Tag struct {
